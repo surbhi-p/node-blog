be81b31dd159b02240ad42a32c70017fa68d2a14
Pandey <2328205@cognizant.com>
2024-09-05 14:09:29 +0530

    initial

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
+  googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
+  mongoURI:
+    "mongodb+srv://new_user01:User@123@cluster0.zbosh.mongodb.net/blog_dev",
+  cookieKey: "123123123",
+};
